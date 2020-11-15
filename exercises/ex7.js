88c0af5c89c2c2479fd2aa144feeb71c63fbebba
Hasham <m.hasham681@gmail.com>
2020-11-15 21:34:35 +0500

    completed digging into node

--- exercises/ex7.js
+++ exercises/ex7.js
@@ -3,13 +3,12 @@
 "use strict";
 
 var util = require("util");
-// var childProc = require("child_process");
-
+var childProc = require("child_process");
 
 // ************************************
 
 const HTTP_PORT = 8039;
-// const MAX_CHILDREN = 5;
+const MAX_CHILDREN = 100;
 
 var delay = util.promisify(setTimeout);
 
@@ -20,5 +19,44 @@ main().catch(console.error);
 // ************************************
 
 async function main() {
-	// console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+	console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+
+	var x = 0;
+
+	while (true) {
+		x ++;
+		process.stdout.write(`Sending ${MAX_CHILDREN} requests ...`);
+
+		let children = [];
+
+		for (let i=0; i<MAX_CHILDREN; i++) {
+			children.push(
+				childProc.spawn("node", [ "ex7-child.js" ])
+			)
+		}
+
+		let resps = children.map(function wait(child) {
+			return new Promise(function(res){
+				child.on('exit', function(code){
+					if (code === 0) res(true);
+					res(false);
+				});
+			});
+		});
+
+		if (x > 5) {
+			foo();
+		}
+
+		resps = await Promise.all(resps);
+
+		if (resps.filter(Boolean).length == MAX_CHILDREN) {
+			console.log('success');
+		}
+		else {
+			console.log('failures.');
+		}
+
+		await delay(500);
+	}
 }
